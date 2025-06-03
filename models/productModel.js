d428a5da292083090bf45e60dcb5c52571c49a38
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-06-03 14:44:25 +0530

    update

--- models/productModel.js
+++ models/productModel.js
@@ -1,9 +1,8 @@
-import mongoose from "mongoose";
-const prodSchema = mongoose.Schema({
-    name: {type:String},
-    des : {type:String},
-    img_Url : {type:String},
-    price:{type:Number},
-})
-//const products = mongoose.model("Products", prodSchema);
-export default mongoose.model("Products", prodSchema)
+import mongoose from 'mongoose'
+const productSchema = mongoose.Schema({
+  name: { type: String },
+  price: { type: Number },
+});
+// const product = mongoose.model("Product", productSchema);
+
+export default mongoose.model("Product", productSchema);
