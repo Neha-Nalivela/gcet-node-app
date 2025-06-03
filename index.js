b987c029fcb18fec313e82de033084712f6ac811
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-06-03 12:38:14 +0530

    update

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 const app = express();
 
 app.listen(8080,()=>{
+    mongoose.connect("mongodb://localhost:27017/gcet");
     console.log("Server Started");
 });
+
+const userSchema = mongoose.Schema({
+    name:{type: String},
+    email: {type: String},
+    pass : {type: String},
+});
+const user = mongoose.model("User",userSchema);
+const prodSchema = mongoose.Schema({
+    name: {type:String},
+    des : {type:String},
+    img_Url : {type:String},
+    price:{type:Number},
+})
+const products = mongoose.model("Products", prodSchema);
+
 app.use(cors());
+app.use(express.json());
 app.get("/",(req,res)=>{
-    return res.send("Good Morning!");
+    return res.send(`<h1>Good Morning!! Welcome to API Page</h1>
+    <ol>
+      <li><a href="/greet">/greet</a></li>
+      <li><a href="/name">/name</a></li>
+      <li><a href="/weather">/weather</a></li>
+      <li><a href="/products">/products</a></li>
+      <li><a href="/register">/register</a></li>
+    </ol>`);
+   
 })
 
 app.get("/greet",(req,res)=>{
-    res.send("Greetings")
+    res.send("Greetings");
 })
 
 app.get("/name",(req,res)=>{
@@ -21,11 +47,26 @@ app.get("/weather",(req,res)=>{
     res.send("32degree");
 })
 
-app.get("/products",(req,res)=>{
-    const products=[
-        {name: "Product1", price: "₹34"},
-        {name: "Product2", price: "₹56"},
-        {name: "Product3", price: "₹42"},
-    ];
-    res.json(products);
+app.get("/products",async (req,res)=>{
+    const product = await products.find(); 
+    res.json(product);
 })
+//get->post
+app.post("/register",async(req,res)=>{
+    const {name, email, pass} = req.body;
+
+    const result = await user.insertOne({name, email, pass});
+    return res.json(result);
+})
+//login
+app.post("/login", async(req,res)=>{
+    const {email, pass} = req.body;
+    const result = await user.findOne({email, pass});
+    if(result){
+        return res.json(result);
+    }
+    else{
+        return res.json({status: "Login failed"});
+    }
+})
+
